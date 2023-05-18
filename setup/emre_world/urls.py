f98f7da949c3c39fe329fa21de675ba0f044916d
enes <enes.barut@gmx.com>
2023-05-18 17:18:14 +0200

    updated the website

---
+++ setup/emre_world/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('reims/', views.reims, name='reims'),
+    path('metallurgie/', views.metallurgie, name='metallurgie'),
+    path('entreprise/', views.entreprise, name='entreprise'),
+]
--- setup/emre_world/views.py
+++ setup/emre_world/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'emre_world/home.html')
+
+def about(request):
+    return render(request, 'emre_world/about.html')
+
+def reims(request):
+    return render(request, 'emre_world/reims.html')
+
+def metallurgie(request):
+    return render(request, 'emre_world/metallurgie.html')
+
+def entreprise(request):
+    return render(request, 'emre_world/entreprise.html')
