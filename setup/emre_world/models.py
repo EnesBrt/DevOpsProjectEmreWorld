ff46145ab56cd96b9c34723f9f12d82e95bf480b
enes <enes.barut@gmx.com>
2023-05-19 21:17:41 +0200

    updated the website

--- setup/emre_world/models.py
+++ setup/emre_world/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    PAGE_CHOICES = (
+        ('about', 'About'),
+        ('reims', 'Reims'),
+        ('metallurgie', 'Metallurgie'),
+        ('entreprise', 'Entreprise'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(default='default-slug', unique=True)
+    content = models.TextField()
+    page = models.CharField(max_length=200, choices=PAGE_CHOICES, default='about')
+
+    def __str__(self):
+        return self.title
--- setup/emre_world/views.py
+++ setup/emre_world/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from .models import Article
 
 def home(request):
     return render(request, 'emre_world/home.html')
 
 def about(request):
-    return render(request, 'emre_world/about.html')
+    articles = Article.objects.filter(page='about')  # Récupérer tous les articles pour la page "About"
+    return render(request, 'emre_world/about.html', {'articles': articles})
 
 def reims(request):
-    return render(request, 'emre_world/reims.html')
+    articles = Article.objects.filter(page='reims')  # Récupérer tous les articles pour la page "Reims"
+    return render(request, 'emre_world/reims.html', {'articles': articles})
 
 def metallurgie(request):
-    return render(request, 'emre_world/metallurgie.html')
+    articles = Article.objects.filter(page='metallurgie')  # Récupérer tous les articles pour la page "Metallurgie"
+    return render(request, 'emre_world/metallurgie.html', {'articles': articles})
 
 def entreprise(request):
-    return render(request, 'emre_world/entreprise.html')
+    articles = Article.objects.filter(page='entreprise')  # Récupérer tous les articles pour la page "Entreprise"
+    return render(request, 'emre_world/entreprise.html', {'articles': articles})
